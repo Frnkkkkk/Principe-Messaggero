b4f82df312e361715b62df0872b734769389a448
Frnkkkkk <107641679+Frnkkkkk@users.noreply.github.com>
2022-06-16 19:37:40 +0200

    Add files via upload

---
+++ PrincipeEMessagero/IlPrincipeEIlMessaggero/IlPrincipeEIlMessaggero/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace IlPrincipeEIlMessaggero
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.Tiger = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.lastmeeting = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 381);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(55, 57);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(12, 316);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(55, 59);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Tiger
+            // 
+            this.Tiger.Interval = 50;
+            this.Tiger.Tick += new System.EventHandler(this.Tiger_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Impact", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 44);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Giorno:";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(-17, 164);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(164, 146);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // lastmeeting
+            // 
+            this.lastmeeting.AutoSize = true;
+            this.lastmeeting.BackColor = System.Drawing.Color.Transparent;
+            this.lastmeeting.Font = new System.Drawing.Font("Impact", 18F, System.Drawing.FontStyle.Italic);
+            this.lastmeeting.Location = new System.Drawing.Point(364, 9);
+            this.lastmeeting.Name = "lastmeeting";
+            this.lastmeeting.Size = new System.Drawing.Size(255, 44);
+            this.lastmeeting.TabIndex = 4;
+            this.lastmeeting.Text = "Ultimo incontro:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1354, 514);
+            this.Controls.Add(this.lastmeeting);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer Tiger;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label lastmeeting;
+    }
+}
+
---
+++ PrincipeEMessagero/IlPrincipeEIlMessaggero/IlPrincipeEIlMessaggero/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IlPrincipeEIlMessaggero
+{
+    public partial class Form1 : Form
+    {
+        bool andata = true,first=true;
+        int YMess=381, XMess=12;
+        int XPrinc=12,YPrinc= 316;
+        int Giorni=1,Distanza=50;
+
+        private void TimerSecond_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Tiger.Start();
+        }
+
+        private void Tiger_Tick(object sender, EventArgs e)
+        {
+            if (first)
+            {
+                pictureBox2.Location = new Point(XPrinc + 5, YPrinc);
+                pictureBox1.Location = new Point(XMess + 5, YMess);
+                XPrinc += 5;
+                XMess += 5;
+                first = false;
+                Giorni++;
+                andata = false;
+                label1.Text = "Giorno: " + Convert.ToString(Giorni);
+                pictureBox1.Load("../../../ezgif.com-rotate.gif");
+            }
+            else
+            {
+
+                if (andata)
+                {
+                    if (XPrinc != XMess)
+                    {
+                        pictureBox1.Location = new Point(XMess + 10, YMess);
+                        XMess += 10;
+                    }
+                    else
+                    {
+                        andata = false;
+                        pictureBox1.Load("../../../ezgif.com-rotate.gif");
+                    }
+                }
+                if(!andata)
+                {
+                    pictureBox1.Location = new Point(XMess - 5, YMess);
+                    XMess -= 5;
+                    if (XMess-5 > 12)
+                    {
+                        pictureBox1.Location = new Point(XMess - 5, YMess);
+                        XMess -= 5;
+                    }
+                    else
+                    {
+                        pictureBox1.Location = new Point(17, YMess);
+                        XMess = 17;
+                        pictureBox1.Load("../../../QGo5isT.gif");
+                        andata = true;
+                    }
+
+                }
+                pictureBox2.Location = new Point(XPrinc + 5, YPrinc);
+                XPrinc += 5;
+                Distanza += 50;
+                // MessageBox.Show(Convert.ToString(XPrinc) + "    " + Convert.ToString(XMess));
+                if (XPrinc == XMess)
+                {
+                    lastmeeting.Text = "Ultimo incontro: giorno " + Giorni.ToString() + ", alla distanza di " + Distanza.ToString()+"Km dal castello";
+                }
+                
+                Giorni++;
+            }
+            label1.Text = "Giorno: " + Convert.ToString(Giorni);
+        }
+    }
+}
